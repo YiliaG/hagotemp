4afc70e8c6df68c18bac660658aead3581d21810
Yiliakuo <gy905155082@163.com>
2017-04-21 18:07:23 +0800

    temp模板

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+/*
+ * Custom code goes here.
+ * A template should always ship with an empty custom.js
+ */
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 3,
+        // autoPlay: true,
+        navigation:true,
+        navigationText:['','']
+    });
+});
