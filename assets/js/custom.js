83e6290849c650f774cf364b88a57e1bedc468c2
Yiliakuo <gy905155082@163.com>
2017-04-24 17:26:48 +0800

    Revert "homepage finished"
    This reverts commit dcb65896cf78a0d541918e77eeea3f4feb8eb405.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,41 +2,12 @@
  * Custom code goes here.
  * A template should always ship with an empty custom.js
  */
-$(function () {
-    var owl = $('.product-carousel');
-    owl.owlCarousel({
-        nav: true,
-        loop: true,
-        navText: ["", ""],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 3
-            },
-            1000: {
-                items: 3
-            }
-        }
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 3,
+        // autoPlay: true,
+        navigation:true,
+        navigationText:['','']
     });
-
-    $('#brands-carousel-1').owlCarousel({
-        nav: true,
-        loop: true,
-        navText: ["", ""],
-        responsive: {
-            0: {
-                items: 2
-            },
-            768: {
-                items: 3
-            },
-            1000: {
-                items: 3
-            }
-        }
-    });
-
-
 });
